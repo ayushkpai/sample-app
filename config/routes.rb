b5656b84ed4432c3ae81720faeda1750dc12e7a7
Ayush Pai <ayushkpai@users.noreply.github.com>
2026-07-23 15:37:16 +0530

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root "static_pages#home"
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  get "help" => "static_pages#help"
+  get "about" => "static_pages#about"
+  get "contact" => "static_pages#contact"
+  get "signup" => "users#new"
 end
