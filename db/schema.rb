b233aeb41becad3442ca4c64dc7701ddc635321d
Ayush Pai <ayushkpai@users.noreply.github.com>
2026-08-24 18:53:58 +0530

    remember me

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_08_15_051346) do
+ActiveRecord::Schema[8.0].define(version: 2026_08_24_131104) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
